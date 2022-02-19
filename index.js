8974a6e89925a0502935638ce5fd3a649f70250f
Paul Gottinger <pgottinger@outlook.com>
2022-02-19 00:24:43 -0600

    markdown generatig with all sections

--- index.js
+++ index.js
@@ -16,6 +16,60 @@ const questions = [
     name: "description",
     message: "Please describe your project",
   },
+
+  {
+    type: "input",
+    name: "installation",
+    message: "Please describe how to install your project",
+  },
+
+  {
+    type: "input",
+    name: "usage",
+    message: "Please describe how to use your project",
+  },
+
+  {
+    type: "list",
+    name: "license",
+    message: "Chose the license for this project: ",
+    choices: [
+        "Apache",
+        "Academic",
+        "GNU",
+        "ISC",
+        "MIT",
+        "Mozilla",
+        "Open"
+    ]
+    },
+
+    {
+    type: "input",
+    name: "contributing",
+    message: "Who are the contributors of this projects?"
+    },
+    {
+    type: "input",
+    name: "tests",
+    message: "Is there a test included with this project?"
+   },
+    {
+    type: "input",
+    name: "questions",
+    message: "What do I do if I have an issue? "
+    },
+    {
+    type: "input",
+    name: "username",
+    message: "Please enter your GitHub username: "
+    },
+    {
+    type: "input",
+    name: "email",
+    message: "Please enter your email: "
+    }
+
 ];
 
 // TODO: Create a function to write README file
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -15,62 +15,54 @@ const generateMarkdown = (userInput) => {
   return `
   # ${userInput.title}
 
-  Simple overview of use/purpose.
+  ![badge](https://img.shields.io/badge/license-${userInput.license}-brightgreen)<br />
   
   ## Description
   
   ${userInput.description}
   
-  ## Getting Started
-  
-  ### Dependencies
-  
-  * Describe any prerequisites, libraries, OS version, etc., needed before installing program.
-  * ex. Windows 10
-  
-  ### Installing
-  
-  * How/where to download your program
-  * Any modifications needed to be made to files/folders
-  
-  ### Executing program
-  
-  * How to run the program
-  * Step-by-step bullets
+  ## Table of Contents 
 
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
   
-  ## Help
+  ## Installation
+
+  ${userInput.installation}
   
-  Any advise for common problems or issues.
- 
+  ## Usage
   
-  ## Authors
-  
-  Contributors names and contact info
-  
-  ex. Dominique Pizzie  
-  ex. [@DomPizzie](https://twitter.com/dompizzie)
-  
-  ## Version History
-  
-  * 0.2
-      * Various bug fixes and optimizations
-      * See [commit change]() or See [release history]()
-  * 0.1
-      * Initial Release
+  ${userInput.usage}
   
   ## License
+
+  ![badge](https://img.shields.io/badge/license-${userInput.license}-brightgreen)
+<br />
+
+ This project is covered by the ${userInput.license} license.
   
-  This project is licensed under the [NAME HERE] License - see the LICENSE.md file for details
+  ## Contributing
   
-  ## Acknowledgments
+  ${userInput.contributing}
+
+  ## Tests
+
+  ${userInput.tests}
+
+  ## Questions
+
+  ${userInput.questions}<br />
+
+  Find me on Github: [${userInput.username}](https://github.com/${userInput.username})<br />)
+
+  Email me with any question about my project at: ${userInput.email}
   
-  Inspiration, code snippets, etc.
-  * [awesome-readme](https://github.com/matiassingers/awesome-readme)
-  * [PurpleBooth](https://gist.github.com/PurpleBooth/109311bb0361f32d87a2)
-  * [dbader](https://github.com/dbader/readme-template)
-  * [zenorocha](https://gist.github.com/zenorocha/4526327)
-  * [fvcproductions](https://gist.github.com/fvcproductions/1bfc2d4aecb01a834b46)`;
+ `;
 };
 
 module.exports = generateMarkdown;
